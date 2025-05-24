ac906ef8b567d8b20ea514471b947df4e4dd142d
RadovanMe <47661616+RadovanMe@users.noreply.github.com>
2025-05-24 11:03:31 +0200

    added wait_for_db and corrected linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,12 +1,11 @@
 """
 Sample tests
-"""
 from django.test import SimpleTestCase
 from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    """test the calc module"""
+    test the calc module
 
     def test_add_numbers(self):
         res = calc.add(5, 6)
@@ -15,3 +14,4 @@ class CalcTests(SimpleTestCase):
     def test_subtract_numbers(self):
         res = calc.subtract(15, 10)
         self.assertAlmostEqual(res, 5)
+"""
