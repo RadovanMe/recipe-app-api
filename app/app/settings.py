9d135f7dba9cd99f5b8b111ac66fd80115ce59c2
RadovanMe <47661616+RadovanMe@users.noreply.github.com>
2025-05-30 23:10:16 +0200

    added drf spectacular - working

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,8 +39,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
+    'drf_spectacular',
 ]
+
+# SPECTACULAR_SETTINGS = {
+#     'TITLE': 'RECIPE API',
+#     'DESCRIPTION': 'Your project description',
+#     'VERSION': '1.0.0',
+#     #'SERVE_INCLUDE_SCHEMA': False,
+#     # OTHER SETTINGS
+# }
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -132,3 +144,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
